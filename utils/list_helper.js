6bb2d939e184e280316431f6e7b3c7ea8c840038
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2020-10-01 16:24:31 +0200

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,15 @@ const dummy = blogs => 1
 
 const totalLikes = blogs => blogs.reduce((acc, cur) => acc + cur.likes, 0)
 
+const favoriteBlog = blogs => {
+  const result = [...blogs].sort((a, b) => b.likes - a.likes)[0]
+  return result
+    ? { title: result.title, author: result.author, likes: result.likes }
+    : {}
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
